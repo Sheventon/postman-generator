67b32febec96fe1b0882a324e7ce47009764268e
Антон Шеверда <sheverda@technokratos.com>
2024-04-23 11:19:28 +0300

    Initial commit

---
+++ src/main/java/ru/itis/postmangenerator/PostmanGeneratorApplication.java
@@ -0,0 +1,13 @@
+package tu.itis.postmangenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostmanGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PostmanGeneratorApplication.class, args);
+    }
+
+}
