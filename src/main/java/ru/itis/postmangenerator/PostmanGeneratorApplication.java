b181522a2ca2e31404456ef9c74ee8c66a111799
adelasaripova <adelya19.2001@mail.ru>
2024-04-23 13:36:41 +0300

    generator checking added

--- src/main/java/ru/itis/postmangenerator/PostmanGeneratorApplication.java
+++ src/main/java/ru/itis/postmangenerator/PostmanGeneratorApplication.java
@@ -1,4 +1,4 @@
-package tu.itis.postmangenerator;
+package ru.itis.postmangenerator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/ru/itis/postmangenerator/dto/Event.java
@@ -0,0 +1,45 @@
+package ru.itis.postmangenerator.dto;
+
+import lombok.*;
+import ru.itis.postmangenerator.dto.enumeration.EventStatus;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Event {
+
+    private UUID id;
+
+    @NotNull
+    private Long organizerId;
+
+    private EventStatus status;
+
+    @NotBlank
+    @Size(max = 200)
+    private String title;
+
+    @Size(max = 1000)
+    private String description;
+
+    @NotNull
+    private LocalDate startDate;
+
+    @NotNull
+    private LocalDate endDate;
+
+    @NotEmpty
+    private List<String> attachments;
+
+    @NotEmpty
+    private List<Task> tasks;
+}
